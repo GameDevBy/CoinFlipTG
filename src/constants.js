e8d438166062c68e92c179216cc388ea110debd6
Star-mi <star-mi-7@mail.ru>
2024-09-30 23:23:37 +0300

    Updates

--- src/constants.js
+++
@@ -1,4 +0,0 @@
-export const tabs = ["home", "games"];
-export const choices = ["Heads", "Tails"];
-
-export const createGameShareUrl = (game) => `https://t.me/${process.env.REACT_APP_TG_BOT_NAME}?start=game_${game.gameCode}`
